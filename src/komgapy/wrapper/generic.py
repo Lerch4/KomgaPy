6b216996598e89e47b49d02ed53525ebb07f67e3
Lerch4 <matt.lerch@gmail.com>
2024-06-15 19:13:37 -0400

    Update generic.py
    Removed test prints

--- src/komgapy/wrapper/generic.py
+++ src/komgapy/wrapper/generic.py
@@ -220,8 +220,6 @@ class Generic(RequestAdapter):
         '''
         endpoint = make_endpoint(item_type, [item_id, 'thumbnail'])
         r = self._get_request(endpoint, {'id': item_id}, headers={'accept': 'image/jpeg'})
-        for i in vars(r):
-            print(i)
         if convert_to_png and not isinstance(r, KomgaErrorResponse):  
             return Image.open(io.BytesIO(r._content))
         else: 
