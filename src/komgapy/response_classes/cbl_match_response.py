c79ede78562d4b400310e11c0acf2838a84fc753
Lerch4 <matt.lerch@gmail.com>
2024-07-08 23:35:43 -0400

    Update cbl_match_response.py
    added skip when no match

--- src/komgapy/response_classes/cbl_match_response.py
+++ src/komgapy/response_classes/cbl_match_response.py
@@ -8,18 +8,45 @@ class cbl_match_response(KomgaResponse):
         self.error_code = match_response_data['errorCode']
         self.book_ids = get_book_ids(self.requests)
     
+# def get_book_ids(requests):
+#     book_ids = []
+#     for request in requests:
+#         if len(request['matches'])>1:
+#             print('multiple matches -- selecting first option')
+        
+
+#         matched_request = request['matches'][0]
+
+#         if len(matched_request['books'])> 1:
+#             print('multiple books -- selecting first option')
+ 
+#         book_ids.append(matched_request['books'][0]['bookId'])
+
+#     return book_ids
+
 def get_book_ids(requests):
     book_ids = []
     for request in requests:
-        if len(request['matches'])>1:
-            print('multiple matches -- selecting first option')
+
+        match len(request['matches']):
+            case 0:
+                print('No Match')
+                continue
+            case 1: pass
+            case _:
+                print('multiple matches -- selecting first option')                
 
         matched_request = request['matches'][0]
 
-        if len(matched_request['books'])> 1:
-            print('multiple books -- selecting first option')
+        match len(matched_request['books']):
+            case 0:
+                print('No Match')
+                continue
+                
+            case 1: pass
+            case _:
+                print('multiple books -- selecting first option')  
  
         book_ids.append(matched_request['books'][0]['bookId'])
 
     return book_ids
-
